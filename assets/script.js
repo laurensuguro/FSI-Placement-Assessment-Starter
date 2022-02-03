2a82961c3ad923530b857a380a7e2e771d40db06
Lauren Suguro <lsuguro@Laurens-MacBook-Pro.local>
2022-02-03 13:12:34 -0500

    added loop

--- assets/script.js
+++ assets/script.js
@@ -4,92 +4,49 @@ console.log("JavaScript code has loaded!");
 let yourName = "Lauren"; // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0; // Gingerbread
-let cc = 0; // Chocolate Chip
-let sugar = 0; // Sugar Sprinkle
-let allCookies = 0;
+let cookieType = [
+  { type: "gb", quantity: 0 },
+  { type: "cc", quantity: 0 },
+  { type: "sugar", quantity: 0 },
+];
+
+function cookieSum() {
+  return (
+    cookieType[0].quantity + cookieType[1].quantity + cookieType[2].quantity
+  );
+}
 
 // selecting the element with an id of credit: header 2
 const credit = document.querySelector("#credit");
 // Cookie buttons
-const gbPlusBtn = document.querySelector("#add-gb");
-let gbMinusBtn = document.querySelector("#minus-gb");
-let ccPlusBtn = document.querySelector("#add-cc");
-let ccMinusBtn = document.querySelector("#minus-cc");
-let sugarPlusBtn = document.querySelector("#add-sugar");
-let sugarMinusBtn = document.querySelector("#minus-sugar");
+let plusBtns = document.querySelectorAll(".plus-button");
+let minusBtns = document.querySelectorAll(".minus-button");
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`;
 
 //Cookie Summary Quantity
-let gbTotal = document.querySelector("#qty-gb");
-let ccTotal = document.querySelector("#qty-cc");
-let sugarTotal = document.querySelector("#qty-sugar");
+let qtySummary = document.querySelectorAll(".qty-summary");
 let cookieTotal = document.querySelector("#qty-total");
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener("click", function () {
-  gb = gb + 1; //add 1 to the gb total
-  gbTotal.textContent = gb;
-  allCookies = gb + cc + sugar;
-  cookieTotal.textContent = allCookies;
-});
+// Event listener for clicks on the "+" button for all cookies
+for (let i = 0; i < plusBtns.length; i++) {
+  plusBtns[i].addEventListener("click", function () {
+    cookieType[i].quantity = cookieType[i].quantity + 1;
+    qtySummary[i].textContent = cookieType[i].quantity;
+    cookieTotal.textContent = cookieSum();
+  });
+}
 
-// Event listener for clicks on the "-" button for Gingerbread cookies
-gbMinusBtn.addEventListener("click", function () {
-  if (gbTotal.textContent > 0) {
-    gb = gb - 1; //subtract 1 to the gb total
-    gbTotal.textContent = gb;
-    allCookies = gb + cc + sugar;
-    cookieTotal.textContent = allCookies;
-  } else {
-    gbTotal.textContent = "0";
-    allCookies = gb + cc + sugar;
-    cookieTotal.textContent = allCookies;
-  }
-});
-
-// Event listener for clicks on the "+" button for chocolate chip cookies
-ccPlusBtn.addEventListener("click", function () {
-  cc = cc + 1; //add 1 to the gb total
-  ccTotal.textContent = cc;
-  allCookies = gb + cc + sugar;
-  cookieTotal.textContent = allCookies;
-});
-
-// Event listener for clicks on the "-" button for chocolate chip cookies
-ccMinusBtn.addEventListener("click", function () {
-  if (ccTotal.textContent > 0) {
-    cc = cc - 1; //add 1 to the gb total
-    ccTotal.textContent = cc;
-    allCookies = gb + cc + sugar;
-    cookieTotal.textContent = allCookies;
-  } else {
-    ccTotal.textContent = "0";
-    allCookies = gb + cc + sugar;
-    cookieTotal.textContent = allCookies;
-  }
-});
-
-// Event listener for clicks on the "+" button for sugar cookies
-sugarPlusBtn.addEventListener("click", function () {
-  sugar = sugar + 1; //add 1 to the gb total
-  sugarTotal.textContent = sugar;
-  allCookies = gb + cc + sugar;
-  cookieTotal.textContent = allCookies;
-});
-
-// Event listener for clicks on the "-" button for chocolate chip cookies
-sugarMinusBtn.addEventListener("click", function () {
-  if (sugarTotal.textContent > 0) {
-    sugar = sugar - 1; //add 1 to the gb total
-    sugarTotal.textContent = sugar;
-    allCookies = gb + cc + sugar;
-    cookieTotal.textContent = allCookies;
-  } else {
-    sugarTotal.textContent = "0";
-    allCookies = gb + cc + sugar;
-    cookieTotal.textContent = allCookies;
-  }
-});
+// Event listener for clicks on the "-" button for all cookies
+for (let i = 0; i < minusBtns.length; i++) {
+  minusBtns[i].addEventListener("click", function () {
+    if (cookieType[i].quantity > 0) {
+      cookieType[i].quantity = cookieType[i].quantity - 1;
+      qtySummary[i].textContent = cookieType[i].quantity;
+      cookieTotal.textContent = cookieSum();
+    } else {
+      qtySummary[i].textContent = 0;
+    }
+  });
+}
